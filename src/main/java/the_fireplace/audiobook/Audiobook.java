ca4ea6b107bf220d7f880755eac83dd2f3d1f836
The_Fireplace <the.f1repl4ce@gmail.com>
2020-09-13 20:11:50 -0500

    Give the cancel audio key a default keybind

--- src/main/java/the_fireplace/audiobook/Audiobook.java
+++ src/main/java/the_fireplace/audiobook/Audiobook.java
@@ -3,7 +3,6 @@ package the_fireplace.audiobook;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.options.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 
 import java.awt.event.KeyEvent;
 
@@ -15,6 +14,6 @@ public class Audiobook implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		audiobookKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.audiobook.read_book", KeyEvent.VK_H, "key.categories.misc"));
-		stopAudiobookKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.audiobook.stop_book", InputUtil.UNKNOWN_KEY.getCode(), "key.categories.misc"));
+		stopAudiobookKey = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.audiobook.stop_book", KeyEvent.VK_J, "key.categories.misc"));
 	}
 }
