aa737dfb2cf1c0be651daf4a8fbd166b25c6360a
The_Fireplace <git@the-fireplace.dev>
2025-06-07 22:56:33 -0500

    [1.20.4] Port

--- forge/src/main/java/dev/the_fireplace/audiobook/entrypoints/Forge.java
+++ forge/src/main/java/dev/the_fireplace/audiobook/entrypoints/Forge.java
@@ -1,14 +1,14 @@
 package dev.the_fireplace.audiobook.entrypoints;
 
 import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("audiobook")
 public class Forge
 {
-    public Forge() {
+    public Forge(FMLJavaModLoadingContext context) {
         // Register as optional on both sides
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
+        context.registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> IExtensionPoint.DisplayTest.IGNORESERVERONLY, (a, b) -> true));
     }
 }
