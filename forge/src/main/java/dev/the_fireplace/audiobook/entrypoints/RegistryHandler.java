fcfa8fad72fa66720379c57ee20a234ba1273907
The_Fireplace <git@the-fireplace.dev>
2025-06-07 22:01:00 -0500

    [1.19.2] Port

--- forge/src/main/java/dev/the_fireplace/audiobook/entrypoints/RegistryHandler.java
+++ forge/src/main/java/dev/the_fireplace/audiobook/entrypoints/RegistryHandler.java
@@ -3,10 +3,9 @@ package dev.the_fireplace.audiobook.entrypoints;
 import dev.the_fireplace.audiobook.AudiobookConstants;
 import dev.the_fireplace.audiobook.AudiobookKeys;
 import net.minecraft.client.KeyMapping;
-import net.minecraftforge.client.ClientRegistry;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 import java.awt.event.KeyEvent;
 
@@ -14,10 +13,10 @@ import java.awt.event.KeyEvent;
 public final class RegistryHandler
 {
     @SubscribeEvent
-    public static void registerBindings(FMLClientSetupEvent event) {
+    public static void registerBindings(RegisterKeyMappingsEvent event) {
         AudiobookKeys.audiobookKey = new KeyMapping("key.audiobook.read_book", KeyEvent.VK_H, "key.categories.misc");
         AudiobookKeys.stopAudiobookKey = new KeyMapping("key.audiobook.stop_book", KeyEvent.VK_J, "key.categories.misc");
-        ClientRegistry.registerKeyBinding(AudiobookKeys.audiobookKey);
-        ClientRegistry.registerKeyBinding(AudiobookKeys.stopAudiobookKey);
+        event.register(AudiobookKeys.audiobookKey);
+        event.register(AudiobookKeys.stopAudiobookKey);
     }
 }
